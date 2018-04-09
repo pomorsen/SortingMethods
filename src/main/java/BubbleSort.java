1d05f6bbe56cf70917e83e13b69ace3797b8cd2a
pmr <pomorpomor@gmail.com>
2018-04-09 19:08:57 +0200

    Bubble sort

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,8 +1,37 @@
+import java.util.List;
+
 /**
- * Created by ppomorsk on 09/04/2018.
+ * procedure bubbleSort( A : lista elementów do posortowania )
+ * n = liczba_elementów(A)
+ * do
+ * for (i = 0; i < n-1; i++) do:
+ * if A[i] > A[i+1] then
+ * swap(A[i], A[i+1])
+ * end if
+ * end for
+ * n = n-1
+ * while n > 1
+ * end procedure
  */
 public class BubbleSort {
     public static void sort(int[] tablica) {
-        
+        int n = tablica.length;
+        int temp = 0;
+        int licznikOperacji = 0;
+        do {
+            for (int i = 0; i < n - 1; i++) {
+                licznikOperacji++;
+                if (tablica[i] > tablica[i + 1]) {
+                    temp = tablica[i];
+                    tablica[i] = tablica[i + 1];
+                    tablica[i + 1] = temp;
+                }
+
+            }
+            n = n - 1;
+        } while (n > 1);
+        System.out.println("Ilość wykonanych operacji " + licznikOperacji);
     }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
+
 /**
  * Created by ppomorsk on 09/04/2018.
  */
 public class Main {
     public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] tablicaLiczb = new int[]{1,2,3,5,6,10,0,4};
+
+
+        System.out.println("Nieposortowana tablica:");
+        System.out.println(Arrays.toString(tablicaLiczb));
+
+        System.out.println("\nPosorotwana tablica:");
+        bubbleSort.sort(tablicaLiczb);
+        System.out.println(Arrays.toString(tablicaLiczb));
+
 
     }
 }
